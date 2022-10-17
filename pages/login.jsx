685d58215e8b8b82f9773d360335d99276abf7ed
santhosh-cloud <56725056+santhosh-cloud@users.noreply.github.com>
2022-10-17 21:10:56 +0530

    authencation done

---
+++ pages/login.jsx
@@ -0,0 +1,20 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+
+export default function Home() {
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <div>
+        Welcome user
+        <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </div>
+    );
+  }
+  return (
+    <div>
+      Click to sign into your user account <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </div>
+  );
+}
